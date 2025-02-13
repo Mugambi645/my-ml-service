1ab52bdacc11aeff64ee7c9e50639c64889a67bc
patrick mugambi <patrick@kali.patrick>
2025-02-13 18:45:52 +0300

    add predict view and tests

--- backend/server/endpoints/urls.py
+++ backend/server/endpoints/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
-from endpoints.views import EndpointViewSet
-from endpoints.views import MLAlgorithmViewSet
-from endpoints.views import MLAlgorithmStatusViewSet
-from endpoints.views import MLRequestViewSet
+from endpoints.views import (
+    EndpointViewSet,
+    MLAlgorithmViewSet,
+    MLAlgorithmStatusViewSet,
+    MLRequestViewSet,
+    PredictView,
+)
 
+# Initialize the router
 router = DefaultRouter(trailing_slash=False)
 router.register(r"endpoints", EndpointViewSet, basename="endpoints")
 router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
@@ -14,4 +18,5 @@ router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
+    re_path(r"^api/v1/(?P<endpoint_name>.+)/predict$", PredictView.as_view(), name="predict"),
 ]
